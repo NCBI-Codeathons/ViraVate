fb24d274ffdbfadf83132881b50b70f053e9a85f
jampowder <jamesleechc@hotmail.com>
2020-01-10 14:58:05 -0500

    added file that creates M and L files for GSECA

---
+++ gseca/create_input.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+path = sys.argv[1]
+
+for filename in os.listdir(path):
+	out_file_m = open(filename.rsplit('.', 1)[0] + "_M.gmt", 'w')
+	out_file_l = open(filename.rsplit('.', 1)[0] + "_L.gmt", 'w')
+	line = open(path + "/" + filename).readline().rstrip('\n').split('\t')
+	for case in line:
+		if case == "infected":
+			out_file_l.write("CASE" + '\n')
+		elif case == "control":
+			out_file_l.write("CNTR" + '\n')
+		else:
+			out_file_l.write(case + '\n')
+	out_file_l.close()
+	with open(path + "/" + filename, 'r') as txt:
+		for gene in txt:
+			out_file_m.write(gene)
+	out_file_m.close()
